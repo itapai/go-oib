3280cdd9c10088efd0113e35d7de1f2d035501c7
Istvan Tapai <itapaik@gmail.com>
2022-11-19 09:15:07 +0100

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/itapai/go-oib
+
+go 1.19
---
+++ oib.go
@@ -0,0 +1,58 @@
+package oib
+
+import (
+	"fmt"
+	"math/rand"
+	"regexp"
+	"strconv"
+)
+
+func IsValid(code string) error {
+	match, err := regexp.MatchString("^[0-9]{11}$", code)
+	if err != nil {
+		return fmt.Errorf("invalid oib: %s", err)
+	}
+
+	if !match {
+		return fmt.Errorf("invalid oib: bad format")
+	}
+
+	checksum := Checksum(code)
+	lastDigit, _ := strconv.Atoi(string(code[10]))
+
+	if checksum != lastDigit {
+		return fmt.Errorf("invalid oib: bad checksum")
+	}
+
+	return nil
+}
+
+func Checksum(code string) int {
+	checksum := 10
+
+	for i := 0; i < 10; i++ {
+		n, _ := strconv.Atoi(string(code[i]))
+
+		checksum += n
+		checksum %= 10
+		if checksum == 0 {
+			checksum = 10
+		}
+
+		checksum *= 2
+		checksum %= 11
+	}
+
+	checksum = 11 - checksum
+	if checksum == 10 {
+		checksum = 0
+	}
+
+	return checksum
+}
+
+func New() string {
+	s := fmt.Sprintf("%010d", rand.Int63n(1e10))
+
+	return fmt.Sprintf("%s%d", s, Checksum(s))
+}
